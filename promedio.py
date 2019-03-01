fed8f16a54045745cbe77cdb586400e8a47f3cf3
Facultad de Ing Sistemas e informatica <informatica@bga.upb>
2019-03-01 14:37:17 -0500

    promedio btw

---
+++ promedio.py
@@ -0,0 +1,14 @@
+print("Python promedio")
+a =(input("Primer valor"))
+b =(input("Segundo valor"))
+c =(input("Tercer valor"))
+d =(input("Cuarto valor"))
+
+e = int(a+b+c+d)
+
+print("total de la suma es:"+str(e));
+input
+
+promedio = (e/4)
+
+print("promedio es:"+str(promedio))
